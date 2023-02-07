a8ef897d90c509306dc9e8050cb02ec248f55587
Rebecca <rebjanec@gmail.com>
2023-02-06 21:50:03 -0500

    again

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "iyd" : "/",
-  outputDir: "iyd",
+  publicPath: process.env.NODE_ENV === "production" ? "dist" : "/",
+  outputDir: "dist",
   configureWebpack: {
     devServer: {
       historyApiFallback: true,
